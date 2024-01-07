79f58f773c93523f9f4c8e922f42b9134d5f8c9a
shishir610 <shishir.srik@gmail.com>
2024-01-07 19:20:27 +0530

    commit via cmd+enter

--- src/components/LeftPanel/LeftPanel.js
+++ src/components/LeftPanel/LeftPanel.js
@@ -1,31 +1,30 @@
-import React, { Fragment } from 'react';
-import { useSelector } from 'react-redux';
-import ListItem from './ListItem'
-import List from './List'
+import React, { Fragment } from "react"
+import { useSelector } from "react-redux"
+import ListItem from "./ListItem"
+import List from "./List"
 
 const constructListItem = (item, itemNumber) => {
     if (item.children.length !== 0) {
         return (
             <List item={item} itemNumber={itemNumber}>
-                {item.children.map((item) => constructListItem(item, itemNumber + 1))}
+                {item.children.map((item) =>
+                    constructListItem(item, itemNumber + 1)
+                )}
             </List>
         )
-    }
-    else {
+    } else {
         return <ListItem item={item} itemNumber={itemNumber} />
     }
 }
 
 const LeftPanel = () => {
-    const FILE_STRUCTURE = useSelector(state => state.filesystem)
+    const FILE_STRUCTURE = useSelector((state) => state.filesystem)
     return (
         <div className="bg-grey-100 xl:w-1/5 min-w-max h-auto hidden md:block">
-            <p className="text-grey-300 p-8 pb-4 hover:bg-grey-200">
-                ROOT
-            </p>
+            <p className="text-grey-300 p-8 pb-4 hover:bg-grey-200">ROOT</p>
             {FILE_STRUCTURE.map((item) => constructListItem(item, 0))}
         </div>
-    );
+    )
 }
 
-export default LeftPanel;
+export default LeftPanel
